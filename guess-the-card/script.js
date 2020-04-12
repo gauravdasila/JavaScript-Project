1273369f3e4c9bd51bc15d48fb6d55992469a283
gauravdasila <gauravdasila07@gmail.com>
2020-04-12 19:30:51 +0530

    c

--- guess-the-card/script.js
+++ guess-the-card/script.js
@@ -1,8 +1,4 @@
-document.querySelector(".spread").addEventListener("click", shuffle);
-document.querySelector(".overlay").addEventListener("mouseover",removeOverlay);
-
-let deck = document.querySelectorAll(".deck");
-let cardArr = [
+let cardData = [
     {
         cardText: "K",
         cardId: "&#9827;"
@@ -22,28 +18,28 @@ let cardArr = [
     {
         cardText: "10",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "9",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "8",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "7",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "6",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "5",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "4",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "3",
         cardId: "&#9827;"
-    },{
+    }, {
         cardText: "2",
         cardId: "&#9827;"
     },
@@ -205,105 +201,10 @@ let cardArr = [
     },
 ];
 
-
-function shuffle() {
-    let card = document.querySelectorAll(".deck-wrapper .card");
-    let randomArr = [];
-    for(let i = 0 ; i < deck.length; i++) {
-        deck[i].addEventListener("mouseover", hoverDeck);
-        deck[i].addEventListener("click", selectedDeck);
-    }  
-    for(let j = 0 ; j < card.length ;j++) {
-        let randomNum = Math.floor(Math.random() * cardArr.length);
-        if(randomArr.length > 1) {
-            function noRepeating() {
-                randomArr.forEach(function(item, index) {
-                    if(randomArr.indexOf(randomNum) != -1) {
-                        randomNum = Math.floor(Math.random() * cardArr.length); 
-                        noRepeating();
-                    } 
-                });
-            }   
-            noRepeating();
-        }
-        randomArr.push(randomNum);
-        if(cardArr[randomNum].cardId == "&#9830;" || cardArr[randomNum].cardId == "&#9829;") {
-            card[j].querySelector(".logo").innerHTML = cardArr[randomNum].cardId;
-            card[j].style.color = "#ff0000";
-        } else {
-            card[j].querySelector(".logo").innerHTML = cardArr[randomNum].cardId;
-            card[j].style.color = "#000";
-        }
-        let cardInfo = card[j].querySelectorAll(".cardIdInfo");   
-        for(let k = 0 ; k < cardInfo.length ; k++) {
-            let cardInfoPara = cardInfo[k].querySelectorAll("p");    
-            for(let z = 0 ; z < cardInfoPara.length; z++) {
-                if(z == 1) {
-                    cardInfoPara[z].innerHTML = cardArr[randomNum].cardId;
-                } else {
-                    cardInfoPara[z].innerHTML = cardArr[randomNum].cardText;   
-                }
-            } 
-        }
-    } 
-    shuffleSound();
-} 
-
-let possibleDeck = [];
-let turn = 0;
-
-function selectedDeck() {
-    let item = this.children;
-    turn++;
-    if(possibleDeck.length == 0) {
-        for(let z = 0 ; z < item.length ;z++) {
-            possibleDeck.push(item[z]);
-        }
-    }
-    deckShuffle.call(this, possibleDeck);
-    let f = 0;
-    let card = document.querySelectorAll(".deck-wrapper .card");
-    for(let i = 0 ; i < 6 ; i++) {
-        for(let j = deck.length ; j > 0  ; j--) {
-            deck[j-1].appendChild(card[f]);
-            f++;
-        }
-    }    
-}
-
-
-function deckShuffle(possibleDeck) {
-    let flag = [];
-    if(turn >= 2) {
-        let ele = this.children;
-        for(let i = 0 ; i < ele.length ; i++) {
-            for(let j = 0 ;j < possibleDeck.length ; j++) {
-                if(ele[i] == possibleDeck[j]) {
-                    flag.push(ele[i]);
-                }
-            }
-        }
-    }
-    if(flag.length == 1) {
-        showResult(flag[0]);
-    } else if(flag.length == 2) {
-        possibleDeck = flag;
-    }
-}
-
-function showResult(data) {
-    document.querySelector(".overlay").style.display = "block";    
-    document.querySelector(".container .deck-wrapper").classList.add("hide");
-    document.querySelector(".container .hand").classList.add("hide");
-    document.querySelector(".modal .result").appendChild(data);
-    document.querySelector(".modal").classList.add("show");
-}
-
-function hoverDeck(e) {
+function showOverlay(e) {
     this.style.zIndex = 10;   
     document.querySelector(".overlay").classList.add("show");
 }
-
 function removeOverlay() {
     document.querySelector(".overlay").classList.remove("show");
     for(let i = 0 ; i < deck.length ; i++) {
@@ -311,7 +212,123 @@ function removeOverlay() {
     }    
 }
 
-function shuffleSound() {
-    let sound = new Audio("sounds/Pop Clip In-SoundBible.com-583746573.wav");
-    sound.play();
+
+let cards = document.querySelectorAll(".deck .card");
+const deck = document.querySelectorAll(".deck");
+const input = document.querySelectorAll(".selectionBox input"); 
+
+let randomNumberArray = [];
+let getRandomNumber = () => {
+    var randomNumber = Math.floor(Math.random() * cardData.length);
+    if (randomNumberArray.length > 1) {
+        if (randomNumberArray.indexOf(randomNumber) == -1) {
+            randomNumberArray.push(randomNumber);
+        } else {
+            return getRandomNumber();
+        }
+    } else {
+        randomNumberArray.push(randomNumber);
+    }
+    return randomNumber;
 }
+
+let shuffleCard = () => {
+    randomNumberArray = [];   
+    for (let i = 0; i < cards.length; i++) {
+        let key = getRandomNumber();
+        let cardSuit = cards[i].querySelector(".card_suit");
+        cardSuit.innerHTML = cardData[key].cardId;
+        (cardData[key].cardId == '&#9829;' || cardData[key].cardId == '&#9830;') ?
+        cards[i].style.color = "#ff0000" : cards[i].style.color = "#000000";
+        let cardInfo = cards[i].querySelectorAll(".card_info");
+        cardInfo.forEach(item => {
+            item.querySelector(".card_info-id").innerHTML = cardData[key].cardId;
+            item.querySelector(".card_info-suit").innerHTML = cardData[key].cardText;
+        });
+        document.querySelector(".selectionBox").style.opacity = 1;
+    }
+}
+
+let getDeckCards = function() {
+    let value = this.value;
+    let deckCard = document.querySelectorAll("#"+value+" .card");
+        return Array.from(deckCard).map(item => item);
+}
+
+let changeCards = function() {
+    let cards = document.querySelectorAll(".deck .card");
+    for(var i = cards.length - 1; i >= 0 ; i--) {
+        if(i > 3) {
+            var j = i % 4;
+            deck[j].appendChild(cards[i]);
+        } else {
+            deck[i].appendChild(cards[i]);
+        }
+    }
+}
+
+let showShuffleLoader = () => {
+    document.querySelector(".shuffling").style.display = "block";
+    document.querySelector(".overlay").style.display = "block";
+}
+
+let removeShuffleLoader = () => {
+    document.querySelector(".shuffling").style.display = "none";
+    document.querySelector(".overlay").style.display = "none"; 
+}
+
+let showResult = (userChosenCard = "") => {
+    document.querySelector(".modal").classList.add("show");
+    document.querySelector(".result").append(userChosenCard);
+}
+
+let resetUserChoice = () => {
+    for(let i = 0 ; i < input.length; i++) {
+        input[i].checked = false;
+    }
+}
+
+let prevCards = [], steps = 0;
+let selectedOption = function() {
+    let currCards = getDeckCards.call(this);
+    if(!prevCards.length) {
+        prevCards = [...currCards];
+    } else {
+        var targetCards = [];
+        for(var i = 0 ; i < currCards.length ; i++ ) {
+            for(var j = 0 ; j < prevCards.length ; j++) {
+                if(currCards[i].isSameNode(prevCards[j])) {
+                    targetCards.push(currCards[i]);
+                    steps++;
+                }
+            }
+        }             
+        prevCards = [...targetCards];
+    }
+    if(prevCards.length === 1) {
+        var [userChosenCard] = targetCards;
+        showResult(userChosenCard);
+    } else if(steps > 3) {
+        document.querySelector(".modal h1").textContent = "Opps you choose the wrong deck?";
+        showResult();
+    }
+    showShuffleLoader();
+    setTimeout(() => {
+        changeCards();
+        removeShuffleLoader();
+        resetUserChoice();
+    }, 1000);
+}
+
+function addEvent() {
+    for(var i = 0 ; i < deck.length ; i++  ) {
+        deck[i].addEventListener("mouseover", showOverlay);
+    }
+    document.querySelector(".spread").addEventListener("click", shuffleCard);
+    document.querySelector(".overlay").addEventListener("mouseover",removeOverlay);
+    for(let i = 0 ; i < input.length; i++) {
+        input[i].addEventListener("click", selectedOption)
+    }
+}
+
+addEvent();
